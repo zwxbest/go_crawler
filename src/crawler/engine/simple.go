a57bec07ee5c0125df00d1a9b7ecccf71ec1d3cc
zwxbest <zhangweixiao@live.com>
2018-10-06 23:31:21 +0800

    rename to simpleEngine

--- src/crawler/engine/simple.go
+++ src/crawler/engine/simple.go
@@ -4,8 +4,9 @@ import (
 	"log"
 	"crawler/fetcher"
 )
+type SimpleEngine struct {}
 
-func  Run(seeds ...Request)  {
+func  (e SimpleEngine) Run(seeds ...Request)  {
 	var requests []Request
 	for _, r := range seeds {
 		requests = append(requests,r)
@@ -15,19 +16,28 @@ func  Run(seeds ...Request)  {
 		r := requests[0]
 		requests = requests[1:]
 
-		log.Printf("Fetching %s\n", r.Url)
-		body, e := fetcher.Fetch(r.Url)
-		if e != nil{
+		parseResult,err := worker(r);
+		if err != nil{
 			log.Printf("Fetcher: error fetching url %s: %v",
-				r.Url, e)
+				r.Url, err)
 			continue
 		}
-		parseResult := r.ParserFunc(body)
 		requests = append(requests, parseResult.Requests...)
 		for _, item :=range parseResult.Items{
 			//%v 默认的打印格式
 			log.Printf("Got item %v", item)
 		}
 	}
+}
+
+func worker( r Request) (ParseResult,error) {
+	log.Printf("Fetching %s\n", r.Url)
+	body, e := fetcher.Fetch(r.Url)
+	if e != nil{
+		log.Printf("Fetcher: error fetching url %s: %v",
+			r.Url, e)
+		return ParseResult{},e
+	}
+	return r.ParserFunc(body), nil
 }
 
