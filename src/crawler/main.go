93d9af251398dd43dc56cd6ba5421359cbe73fc4
zwxbest <zhangweixiao@live.com>
2018-10-06 18:43:04 +0800

    init

---
+++ src/crawler/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"io/ioutil"
+)
+
+func main()  {
+	resp, err := http.Get("http://www.zhenai.com/zhenghun")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: status code", resp.StatusCode)
+		return
+	}
+
+	all, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", all)
+
+}
