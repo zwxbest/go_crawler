25aedeb4a7088290d451ee15737c68b1e21b704c
zwxbest <zhangweixiao@live.com>
2018-10-06 20:15:13 +0800

    test pass

--- src/crawler/zhenai/parser/citylist_test.go
+++ src/crawler/zhenai/parser/citylist_test.go
@@ -1,10 +1,10 @@
 package parser
 
 import (
-	"crawler/fetcher"
 	"testing"
-	"fmt"
-)
+		"fmt"
+	"crawler/fetcher"
+	)
 
 func TestParseCityList(t *testing.T) {
 	contents, e := fetcher.Fetch("http://www.zhenai.com/zhenghun")
@@ -12,5 +12,39 @@ func TestParseCityList(t *testing.T) {
 	if e!=nil {
 		panic(e)
 	}
+
 	fmt.Printf("%s\n",contents)
+
+	result := ParseCityList(contents)
+	const resultSize = 470
+
+	expectedUrls :=[]string{
+		"http://www.zhenai.com/zhenghun/aba","http://www.zhenai.com/zhenghun/akesu","http://www.zhenai.com/zhenghun/alashanmeng",
+	}
+	expectedCities :=[]string{
+		"阿坝","阿克苏","阿拉善盟",
+	}
+
+	if len(result.Requests) != resultSize {
+		t.Errorf("result sould have %d requests; but had %d",
+			resultSize, len(result.Requests))
+	}
+
+	for i,url := range expectedUrls {
+		if result.Requests[i].Url != url {
+			t.Errorf("expected url #%d: %s;but "+"was %s",i,url,result.Requests[i].Url);
+		}
+	}
+	if len(result.Items) != resultSize {
+		t.Errorf("result sould have %d requests; but had %d",
+			resultSize, len(result.Items))
+	}
+
+	for i,city := range expectedCities {
+		if result.Items[i].(string) != city {
+			t.Errorf("expected city #%d: %s;but "+"was %s",i,city,result.Items[i].(string));
+		}
+	}
+
+
 }
